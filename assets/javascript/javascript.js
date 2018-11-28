64aae21844e4da48ae4c40528b8510f9cabba3f0
Dave Ellis <dgellis@live.com>
2018-11-27 20:41:06 -0700

    dave JS push cleaned up JS

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -51,14 +51,16 @@ window.onload=function(){
 }
 
 
+
+
         //// **** LISTENERS **** ////
 
-// Button - Run SongKick API based on City & Date
+// Button to run SongKick API, using input City & Date
 $(document).on("click", "#form-run-songkick", function() {
     apiSongKickRun();
 });
 
-// Button - Load video of selected artist event
+// Button to load artist video of selected event
 $(document).on("click", ".button-load-video", function(event) {
     event.preventDefault();
     headlineArtist = $(this).attr("data-artist");
@@ -67,137 +69,141 @@ $(document).on("click", ".button-load-video", function(event) {
 });
 
 
+
         //// **** FUNCTIONS **** ////
 
-// ~~~ SONGKICK ~~~
+//// ~~~ SONGKICK ~~~ ////
 
-// API request - pull events based on location and date
+// API FUNCTION - pull events based on location and date
 function apiSongKickRun() {
 
     // Clear previous API search results
     eventsAllObj = [];
     eventsObj = [];
+    $("#form-results-events").empty();
 
     // New location is from input field
     apiSongKickQuery = $("#form-location-field").val();
   
-    // Run query based on location (e.g. Denver)
+    // Query URL using location name (e.g. Denver)
     var queryURL = "https://api.songkick.com/api/3.0/search/locations.json?query=" + apiSongKickQuery + "&apikey=" + apiSongKickKey;  // api query to identify location ID
 
     console.log("queryURL is " + queryURL);
 
-    // 
+    // Run 1st JSON request, using location name (e.g. Denver) to get location ID (e.g. 6404)
     $.ajax({
       url: queryURL,
       method: "GET"
     })
 
-        .then(function(response) {
+    .then(function(response) {
 
-            // CONVERT INPUT LOCATION TO SONGKICK LOCATION ID
-            response.resultsPage.results.location[0];
-            // console.log(response);
-            // console.log(response.resultsPage.results.location[0].metroArea)
-            locationCity = response.resultsPage.results.location[0].metroArea.displayName;
-            var locationState = "";
-            if (response.resultsPage.results.location[0].metroArea.state != undefined) {
-                locationState = response.resultsPage.results.location[0].metroArea.state.displayName;
-            }
-            locationCountry = response.resultsPage.results.location[0].metroArea.country.displayName;
-            $("#form-results-location").html(locationCity + ", " + locationState + " " + locationCountry)
-            locationLat = response.resultsPage.results.location[0].metroArea.lat;
-            locationLng = response.resultsPage.results.location[0].metroArea.lng;
-            locationID = response.resultsPage.results.location[0].metroArea.id;
-            console.log("locationID is " + locationID);
+        // Grab location details, including location ID (e.g. Denver is 6404), required for JSON search
+        response.resultsPage.results.location[0];
 
-            // RUN QUERY
-            var queryURL = "https://api.songkick.com/api/3.0/metro_areas/" + locationID + "/calendar.json?apikey=" + apiSongKickKey;  // api query using location ID
-
-            console.log(queryURL);
-
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            })
+        locationCity = response.resultsPage.results.location[0].metroArea.displayName;
+        locationID = response.resultsPage.results.location[0].metroArea.id;
+        locationCountry = response.resultsPage.results.location[0].metroArea.country.displayName;
+        locationLat = response.resultsPage.results.location[0].metroArea.lat;
+        locationLng = response.resultsPage.results.location[0].metroArea.lng;
         
-                .then(function(response) {
-                    console.log(response);
-                    for (i = 0; i < response.resultsPage.results.event.length; i++) { // for loop to process all upcoming events into eventsAllObj
-                        if (response.resultsPage.results.event[i].start.date >= "2018-11-20" && response.resultsPage.results.event[i].start.date <= "2018-12-31") {  // specify date range
-                            if (response.resultsPage.results.event[i].flaggedAsEnded == false) {  // check event not ended
-                                if (response.resultsPage.results.event[i].status != "cancelled") {  // check event not cancelled
-                                    // console.log(response.resultsPage.results.event[i]);
-                                    eventsAllObj.push({  // push event details to eventsAllObj array for easier access
-                                        num: "event" + (eventsAllObj.length + 1),
-                                        name: response.resultsPage.results.event[i].displayName,
-                                        venue:  response.resultsPage.results.event[i].venue.displayName,
-                                        venuelocation: response.resultsPage.results.event[i].location.city,
-                                        artist:  response.resultsPage.results.event[i].performance[0].artist.displayName,
-                                        startdate:  response.resultsPage.results.event[i].start.date,
-                                        starttime:  response.resultsPage.results.event[i].start.time,
-                                        url:  response.resultsPage.results.event[i].uri,
-                                    });
-                                }
-                            }
-                        }    
+        // Special steps required for state.  Non-US locations throw an error.
+        var locationState = "";
+        if (response.resultsPage.results.location[0].metroArea.state != undefined) {
+            locationState = response.resultsPage.results.location[0].metroArea.state.displayName;
+        }
+
+        $("#form-results-location").html(locationCity + ", " + locationState + " " + locationCountry)
+
+
+        // Query URL using location ID (e.g. 6404)
+        var queryURL = "https://api.songkick.com/api/3.0/metro_areas/" + locationID + "/calendar.json?apikey=" + apiSongKickKey;
+        
+
+        // Run 2nd JSON request, using location ID (e.g. 6404) to get events in area (array of events)
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+    
+        .then(function(response) {
+            console.log(response);
+
+            // For loop to store all upcoming events into eventsAllObj array
+            for (i = 0; i < response.resultsPage.results.event.length; i++) {
+
+// !!!!! THIS DATE LINE IS INCOMPLETE - MUST PULL DATE FROM INPUT !!!!!
+// Also, can these IFs be combined into a neater code?
+                // Check within date range
+                if (response.resultsPage.results.event[i].start.date >= "2018-11-20" && response.resultsPage.results.event[i].start.date <= "2018-12-31") {
+                    // Check event not ended
+                    if (response.resultsPage.results.event[i].flaggedAsEnded == false) {
+                        // Check event not cancelled
+                        if (response.resultsPage.results.event[i].status != "cancelled") {
+                            // Push events into eventsAllObj array
+                            eventsAllObj.push({
+                                num: "event" + (eventsAllObj.length + 1),
+                                name: response.resultsPage.results.event[i].displayName,
+                                venue:  response.resultsPage.results.event[i].venue.displayName,
+                                venuelocation: response.resultsPage.results.event[i].location.city,
+                                artist:  response.resultsPage.results.event[i].performance[0].artist.displayName,
+                                startdate:  response.resultsPage.results.event[i].start.date,
+                                starttime:  response.resultsPage.results.event[i].start.time,
+                                url:  response.resultsPage.results.event[i].uri,
+                            });
+                        }
                     }
-                    console.log(eventsAllObj);
+                }    
+            }
 
-                    $("#form-results-events").empty();
-                    for (var i = 0; i < 6; i++) {
-                        var x = Math.floor(Math.random()*eventsAllObj.length);
-                        // console.log("x is " + x);
-                        eventsObj.push(eventsAllObj[x]);
-                        eventsAllObj.splice(x, 1);
-                        // console.log(eventsObj[i].num);
-                        // $("#form-results-events").append('\
-                        //     <p>name: ' + eventsObj[i].name + '</p>\
-                        //     <p>artist: ' + eventsObj[i].artist + '</p>\
-                        //     <p>venue: ' + eventsObj[i].venue + '</p>\
-                        //     <p>startdate: ' + eventsObj[i].startdate + '</p>\
-                        //     <button type="button" id="watchvideo">Watch A Video</button>\
-                        //     <br>\
-                        // ');
+            console.log(eventsAllObj);
 
+            // Pull x number of events from local array of events (i.e. eventsAllObj array).
+            // Deletes them from the array so they aren't shown again.
+            // !!!!! IF THE SAME SEARCH IS RUN, PERHAPS CONTINUE TO READ AND REMOVE FROM REMAINING ARRAY.  THIS WILL AVOID DUPLICATES.
+            for (var i = 0; i < 6; i++) {
+                var x = Math.floor(Math.random()*eventsAllObj.length);
+                eventsObj.push(eventsAllObj[x]);
+                eventsAllObj.splice(x, 1);
 
-                    var eventName = eventsObj[i].name.substring(0, eventsObj[i].name.indexOf(' at '));
+                // Cut out extraneous text from event name (i.e. 'Beck at The Paramount (November 27)' becomes 'Beck')
+                var eventName = eventsObj[i].name.substring(0, eventsObj[i].name.indexOf(' at '));
 
-                        $("#form-results-events").append('\
-                            <div class="col-md-4 mb-4">\
-                                <div class="card h-100">\
-                                    <div class="card-body">\
-                                        <h2 class="card-title event-name">' + eventName + '</h2>\
-                                        <p class="card-text event-details">Headlining Artist: ' + eventsObj[i].artist + '</p>\
-                                        <p class="card-text event-details">Venue: ' + eventsObj[i].venue + '</p>\
-                                        <p class="card-text event-details">Start Date: ' + eventsObj[i].startdate + '</p>\
-                                    </div>\
-                                    <div class="card-footer">\
-                                        <a href="#" class="btn btn-primary button-load-video" data-artist="' + eventsObj[i].artist + '">Load Music Video</a>\
-                                    </div>\
-                                </div>\
+                // 
+                $("#form-results-events").append('\
+                    <div class="col-md-4 mb-4">\
+                        <div class="card h-100">\
+                            <div class="card-body">\
+                                <h2 class="card-title event-name">' + eventName + '</h2>\
+                                <p class="card-text event-details">Headlining Artist: ' + eventsObj[i].artist + '</p>\
+                                <p class="card-text event-details">Venue: ' + eventsObj[i].venue + '</p>\
+                                <p class="card-text event-details">Start Date: ' + eventsObj[i].startdate + '</p>\
                             </div>\
-                        ');
-                    }
-                    // console.log(eventsAllObj);
-                    console.log(eventsObj);
+                            <div class="card-footer">\
+                                <a href="#" class="btn btn-primary button-load-video" data-artist="' + eventsObj[i].artist + '">Load Music Video</a>\
+                            </div>\
+                        </div>\
+                    </div>\
+                ');
+            }
 
-                });
 
-     
+            // Prints all remaining events in eventsAllObj array
+            // console.log(eventsAllObj);
+            // Prints all events we've pushed to html
+            // console.log(eventsObj);
 
         });
-    
 
+    });
 
 }
 
 
 
+//// ~~~ YOUTUBE ~~~ ////
 
-
-
-
-
+// API FUNCTION - Get video based on headline artist
 function loadVideo() {
-    // push 'headlineArtist' variable into Youtube API, autoplay video
+    // use variable 'headlineArtist' and Youtube API to get video and autoplay
 }
